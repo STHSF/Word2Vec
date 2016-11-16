a543c10739a0ecff53466a727d53a69e6fc775a3
STHSF <woailiyu@163.com>
2016-11-16 15:59:07 +0800

    add model

---
+++ src/main/scala/Word2Vec.scala
@@ -0,0 +1,279 @@
+import java.io.{File, PrintWriter}
+
+import org.apache.spark.{SparkConf, SparkContext}
+import org.apache.spark.mllib.feature.{Word2VecModel, Word2Vec}
+import org.apache.spark.rdd.RDD
+import utils.{TimeUtil, LoggerUtil, DirectoryUtil}
+
+//import java.util
+
+import scala.collection.mutable
+
+/**
+  * Created by li on 16/7/8.
+  * 使用Word2Vec创建词向量空间, 用于计算词之间的相似度.
+  */
+object Word2Vec {
+
+  /**
+    * 判断文档中是否存在目标内容
+    *
+    * @param punctuation 目标内容
+    * @param text 文档
+    * @return
+    * @author Li Yu
+    * @note rowNum: 6
+    */
+  def punctuationRemove(punctuation: Array[String], text: Array[String]): Boolean = {
+
+    text.foreach { line => {
+      if (punctuation.contains(line)) return true
+    }}
+
+    false
+  }
+
+  /**
+    * 格式转换, 将格式转换成word2Vec需要的输入格式.
+    * 1) 去除长度小于3的异常数据
+    * 2) 标点符号剔除
+    *
+    * @param textLib 词库
+    * @param punctuation 标点
+    * @return
+    * @author Li Yu
+    * @note rowNum: 6
+    */
+  def formatTransform(textLib: RDD[String], punctuation: Array[String]): RDD[Seq[String]] = {
+
+    val result = textLib.map(_.split("\t")).filter(_.length > 3)  //去异常值
+      .map(x => x(3).split(",")) // 去分词结果
+      .map (line => line.filterNot(elem => punctuation.contains(elem)))  // 去标点符号
+      .map(_.toSeq)
+
+    result
+  }
+
+  /**
+    * 训练语料库向量模型
+    * 有一些参数在1.5.2中没有, 例如setWindowSize()
+    *
+    * @param sc SparkContext
+    * @param dir Word2VecModel 模型的保存位置
+    * @param input 语料库
+    * @param seed 搜索深度
+    * @param vectorSize 生成的向量长度
+    * @param minCount 包含改词的最少文档总数
+    * @return
+    * @author Li Yu
+    * @note rowNum: 8
+    */
+  def word2VectorModel(sc: SparkContext, dir: String, input: RDD[Seq[String]], seed: Long, vectorSize: Int, minCount: Int): Unit = {
+
+    val word2vec = new Word2Vec()
+    LoggerUtil.warn("词向量模型训练开始 》》》》》》》》》》》》》")
+
+    val model = word2vec
+      .setSeed(seed)
+      .setMinCount(minCount)
+      .setVectorSize(vectorSize)
+      .fit(input)
+    LoggerUtil.warn("模型训练完毕 》》》》》》》》》》》》》")
+
+    val dirFile = new File(dir)
+
+    if (DirectoryUtil.deleteDir(dirFile)) {
+
+      model.save(sc, dir)
+
+    } else {
+
+      model.save(sc, dir)
+    }
+
+    LoggerUtil.warn("模型保存完毕 》》》》》》》》》》》》》")
+  }
+
+  /**
+    * 将Word2VecModel计算的词向量保存到本地
+    *
+    * @param model Word2VecModel
+    * @param dir Word2VecModel模型的保存位置
+    * @param modelName 待保存的词向量名
+    * @author Li Yu
+    * @note rowNum: 7
+    */
+  def saveVocabularyVectors(model: Word2VecModel, dir: String, modelName: String): Unit = {
+
+    val vectors = model.getVectors.toList
+    LoggerUtil.warn("词向量读取结束 》》》》》》》》》》》》》")
+
+    // 保存到本地
+    val writer = new PrintWriter(new File(dir +"%s".format(modelName) + ".txt"))
+
+    vectors.foreach {
+      line => {
+        writer.write("%s\t%s\n".format(line._1, line._2.mkString(",")))
+      }
+    }
+
+    LoggerUtil.warn("词向量写入结束 》》》》》》》》》》》》")
+    writer.close()
+  }
+
+  /**
+    * 将Word2VecModel计算的词向量保存到HDFS上
+    *
+    * @param model Word2VecModel
+    * @param dir Word2VecModel模型的保存位置
+    * @param modelName 待保存的词向量名
+    * @author Li Yu
+    * @note rowNum: 6
+    */
+  def saveVocabularyVectors2HDFS(sc: SparkContext, model: Word2VecModel, dir: String, modelName: String): Unit = {
+
+    val vectors = model.getVectors
+    LoggerUtil.warn("词向量读取结束 》》》》》》》》》》》》》")
+
+    val vector = vectors.map((line: (String, Array[Float])) => "%s\t%s".format(line._1, line._2.mkString(","))).toList
+
+    // 保存到hdfs上
+    val vectorsRDD = sc.parallelize(vector)
+
+    vectorsRDD.saveAsTextFile("dir")
+
+  }
+
+  /**
+    * 从词向量库中匹配候选单词的词向量
+    * 可能存在的问题, 词库中可能不包含新词,因此新词的词向量是找不到的
+    *
+    * @param vectorLib 词向量库
+    * @param input 候选词
+    * @author Li Yu
+    * @note rowNum: 10
+    */
+  def readVocabularyVector(vectorLib: RDD[(String, Array[Double])],
+                           input: Array[String]): Map[String, Array[Double]] = {
+
+    val result = new mutable.HashMap[String, Array[Double]]
+
+    input.foreach( word => {
+
+      val temp = vectorLib.filter(x => x._1.contains(word))
+
+      if(temp != null) {
+
+        result.put(temp.first()._1, temp.first()._2)
+
+      } else {
+
+        result.put(word, Array(0.0))
+
+      }
+    })
+
+    result.toMap
+  }
+
+
+
+  def findVocabularyVector(vectorLib: RDD[(String, Array[Double])], word: String)
+  :Array[Double] = {
+
+
+    val temp = vectorLib.filter(x => x._1.contains(word))
+
+    if(temp != null) {
+
+      temp.first()._2
+    } else {
+
+      Array(0.0)
+    }
+  }
+
+  /**
+    * 构建词向量
+    * 可能存在的问题, 词库中可能不包含新词,因此新词的词向量是找不到的
+    *
+    * @param model Word2VecModel
+    * @param dir Word2VecModel模型的保存位置
+    * @param input 待构建词库
+    * @return
+    * @author Li Yu
+    * @note rowNum: 10
+    */
+  def createW2VMatrix(model: Word2VecModel , dir: String, input: Array[String]): Array[(String, Array[Double])] = {
+
+    val result = new mutable.HashMap[String, Array[Double]]
+
+    input.foreach { line => {
+
+      if (model.getVectors.contains(line)){
+
+        val temp = model.transform(line)
+        result.put(line, temp.toArray)
+
+      } else {
+
+        result.put(line, Array(0.0))
+      }
+    }}
+
+    result.toArray
+  }
+
+  /**
+    * main函数
+    *
+    * @param args
+    * @author Li Yu
+    * @note rowNum: 15
+    */
+  def main(args: Array[String]) {
+
+    val conf = new SparkConf().setAppName("word2vec").setMaster("local")
+    val sc = new SparkContext(conf)
+
+    // 从hdfs上获取数据
+     val input =sc.textFile("/Users/li/Downloads/part-00000")
+//    val input =sc.textFile(args(0))
+    LoggerUtil.warn("读取hdfs上的数据结束 》》》》》》》》》》》》》")
+
+    // 导入标点符号
+    val punctuation = sc.textFile("/Users/li/kunyan/DataSet/punctuations.txt").collect()
+
+    //去异常值, 去标点符号
+    val inputData = formatTransform(input, punctuation)
+    LoggerUtil.warn("数据转换结束 》》》》》》》》》》》》》文本数量: " + "%s".format(inputData.count()))
+
+    val hour = TimeUtil.getCurrentHour
+    val day = TimeUtil.getDay
+
+//    val dir = args(1) + "%s".format(day) + "-" + "%s".format(hour) + "-word2VectorModel"
+     val dir = "/Users/li/kunyan/DataSet/word2vec/result/" + "%s".format(day) + "-" + "%s".format(hour) + "-word2VectorModel"
+
+    // 创建词向量模型
+//    val r1 = args(2).toLong
+//    val r2 = args(3).toInt
+//    val r3 = args(4).toInt
+    val r1 = 10
+    val r2 = 100
+    val r3 = 50
+    word2VectorModel(sc, dir, inputData, r1, r2, r3)
+    LoggerUtil.warn("词典构建结束 》》》》》》》》》》》》》")
+
+    val model = Word2VecModel.load(sc, dir)
+    LoggerUtil.warn("load模型成功 》》》》》》》》》》》》》")
+
+    //保存词向量
+    saveVocabularyVectors(model, dir, day)
+    LoggerUtil.warn("词向量保存成功 》》》》》》》》》》》》》")
+
+    sc.stop()
+    println("-----------程序结束--------------")
+
+  }
+
+}
---
+++ src/main/scala/utils/TimeUtil.scala
@@ -0,0 +1,142 @@
+package utils
+
+import java.math.BigInteger
+import java.text.SimpleDateFormat
+import java.util.{Calendar, Date}
+
+import org.apache.hadoop.hbase.client.Scan
+import org.apache.hadoop.hbase.protobuf.ProtobufUtil
+import org.apache.hadoop.hbase.protobuf.generated.ClientProtos
+import org.apache.hadoop.hbase.util.Base64
+
+/**
+  * 格式化时间的工具类
+  */
+ object TimeUtil {
+
+
+  /**
+    * 获取时间戳对应的时间
+    * @param timeStamp 时间戳
+    * @return
+    */
+  def getTime(timeStamp: String): String = {
+    val sdf: SimpleDateFormat = new SimpleDateFormat("yyyy-MM-dd_HH:mm:ss")
+    val bigInt: BigInteger = new BigInteger(timeStamp)
+    val date: String = sdf.format(bigInt)
+    date
+  }
+
+  /**
+    * 获取当前时间,并转换成制定的格式
+    * @return
+    */
+  def getDay: String = {
+    val sdf: SimpleDateFormat = new SimpleDateFormat("yyyy-MM-dd")
+    val date: String = sdf.format(new Date)
+    date
+  }
+
+  /**
+    * 获取当前小时
+    * @return
+    */
+  def getCurrentHour: Int = {
+    val calendar = Calendar.getInstance
+    calendar.setTime(new Date)
+    calendar.get(Calendar.HOUR_OF_DAY)
+  }
+
+  /**
+    * 获取当前小时的前一个小时
+    * @return
+    */
+  def getPreHourStr: String = {
+    val date = new Date(new Date().getTime - 60 * 60 * 1000)
+    val sdf: SimpleDateFormat = new SimpleDateFormat("yyyy-MM-dd-HH")
+    sdf.format(date)
+  }
+
+  /**
+    * 获取今天的日期
+    *
+    * @return
+    */
+  def getNowDate(): String = {
+    val now: Date = new Date()
+    val  dateFormat: SimpleDateFormat = new SimpleDateFormat("yyyy-MM-dd")
+    val res = dateFormat.format(now)
+    res
+  }
+
+
+  /**
+    * 获取本周的开始时间
+    */
+  def Null(){
+
+  }
+
+  /**
+    * 获取本月的开始时间
+    * http://blog.csdn.net/springlustre/article/details/47273353
+    */
+
+
+  /**
+    * 设置时间范围
+    *
+    * @return 时间范围
+    * @author
+    */
+  def setTimeRange(): String = {
+
+    val scan = new Scan()
+    val date = new Date(new Date().getTime - 30 * 24 * 60 * 60 * 1000)
+    val format = new SimpleDateFormat("yyyy-MM-dd HH")
+    val time = format.format(date)
+    val time1 = format.format(new Date().getTime)
+    val startTime = time + "-00-00"
+    val stopTime = time1 + "-00-00"
+    val sdf: SimpleDateFormat = new SimpleDateFormat("yyyy-MM-dd HH-mm-ss")
+    val startRow: Long = sdf.parse(startTime).getTime
+    val stopRow: Long = sdf.parse(stopTime).getTime
+
+    scan.setTimeRange(startRow, stopRow)
+    val proto: ClientProtos.Scan = ProtobufUtil.toScan(scan)
+
+    Base64.encodeBytes(proto.toByteArray)
+  }
+
+  /**
+    * 设置指定的时间范围(一天)
+    * @param time 指定的日期
+    * @return 指定日期至前一天时间范围
+    */
+  def setAssignedTimeRange(time: String): String = {
+
+    val format = new SimpleDateFormat("yyyy-MM-dd")
+
+    val date = format.parse(time)
+
+    val endTime = new Date(date.getTime - 24 * 60 * 60 * 1000)
+
+    val stopTime = format.format(endTime)
+
+    val startDate = time + "-00-00-00"
+    val stopDate = stopTime  + "-00-00-00"
+
+    val sdf = new SimpleDateFormat("yyyy-MM-dd HH-mm-ss")
+    val startRaw = sdf.parse(startDate).getTime
+    val stopRaw = sdf.parse(stopDate).getTime
+
+    val scan = new Scan()
+    scan.setTimeRange(startRaw, stopRaw)
+
+    val proto = ProtobufUtil.toScan(scan)
+
+    Base64.encodeBytes(proto.toByteArray)
+  }
+
+
+}
